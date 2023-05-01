b4ad57ab1560d755a5096ddf7f8412d05341873e
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-05-01 23:38:09 +0100

    Implemented switch dragging feature

--- lib/src/riff_switch/text_switch.dart
+++ lib/src/riff_switch/text_switch.dart
@@ -14,6 +14,7 @@ class TextSwitch extends StatefulWidget {
     this.inactiveTrackColor,
     this.inactiveThumbColor,
     this.trackColor,
+    this.thumbColor,
   }) : super(key: key);
 
   /// Whether this switch is on or off.
